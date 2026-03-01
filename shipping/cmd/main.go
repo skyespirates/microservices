aa9e5e55a799332d4a52bbe38871cd4c223b0724
Aziz <akashiseijurou414@gmail.com>
2026-03-01 21:27:58 +0700

    feat: initialize shipping service

---
+++ shipping/cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /shipping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("initialize shipping service"))
+	})
+
+	server := http.Server{
+		Addr:    ":3003",
+		Handler: mux,
+	}
+
+	log.Println("server running on port 3003")
+	log.Println(server.ListenAndServe())
+}
---
+++ shipping/go.mod
@@ -0,0 +1,3 @@
+module github.com/skyespirates/microservices/shipping
+
+go 1.25.4
